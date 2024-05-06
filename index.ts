5f42be591882d8fa2ff18636c834e4e62757cd74
saboorHaider <aitools076@gamil.com>
2024-05-06 23:09:15 +0500

    Adding a Simple CLI Calculator Project

---
+++ index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+console.log(chalk.bgBlueBright("\n\t__________Wellcome To Simple Calculator__________"));
+
+const asnwer = await inquirer.prompt([
+    { message : "Enter first number" , type : "number", name: "firstNumber"},
+    { message : "Enter second number", type : "number", name: "secondNumber" },
+    { message : (chalk.green("Select one of the operators to perform operation")),
+    type:"list",
+    name:"operator",
+    choices : [ "Addition" , "Subtraction" , "Multiplication" , "Division"]
+},
+]);
+
+if (asnwer.operator === "Addition")
+{
+    console.log ("Your value is", asnwer.firstNumber + asnwer.secondNumber);
+} else if (asnwer.operator === "Subtraction")
+{ 
+    console.log ("Your value is", asnwer.firstNumber - asnwer.secondNumber);
+} else if (asnwer.operator === "Multiplication") 
+{
+    console.log ("Your value is", asnwer.firstNumber * asnwer.secondNumber);
+} else if (asnwer.operator === "Division")
+{
+    console.log ("Your value is", asnwer.firstNumber/asnwer.secondNumber);
+} 
+else
+{
+        console.log(chalk.red("Please Select Valid Operater"))
+}
